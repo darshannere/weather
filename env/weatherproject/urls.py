80a04099c38493a0636863e7c4cd9f102e058d19
Darshan <73795559+darshannere@users.noreply.github.com>
2021-08-28 21:16:45 +0530

    for hosting

--- env/weatherproject/urls.py
+++ env/weatherproject/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.views.static import serve
+from django.conf.urls import url
+from django.conf import settings
 
 
 urlpatterns = [
@@ -23,5 +26,7 @@ urlpatterns = [
 
     path('',include('first.urls')),
     path('admin/', admin.site.urls),
+     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     
 ]
