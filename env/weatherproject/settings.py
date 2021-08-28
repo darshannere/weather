ddbd01af5e7f87311c3fa8634e7fabcc9786a0fe
Darshan <73795559+darshannere@users.noreply.github.com>
2021-08-28 21:26:50 +0530

    adding domain

--- env/weatherproject/settings.py
+++ env/weatherproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-okavtr%ft0h_6!zica1^#@wac^l!@71&ea7c3m&85_f75_==gi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://127.0.0.1:8000/']
+ALLOWED_HOSTS = ['127.0.0.1','weatherdjangodr.herokuapp.com']
 
 
 # Application definition
